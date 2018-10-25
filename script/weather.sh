a3fbf6f941da3d2be7628a2dc874b826ea794385
Romain THERRAT <romain42@gmail.com>
2018-10-25 11:44:29 +0200

    prevent quota rate limit

--- script/weather.sh
+++ script/weather.sh
@@ -1,3 +1,4 @@
-temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }' )
+#temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }' )
+temp=32
 
 echo -n "weather,city=clermont-ferrand temp=$temp"
