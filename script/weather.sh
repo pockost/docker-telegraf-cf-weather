b724eaa578575f6b7863c8a51aebab08836d81d9
Romain THERRAT <romain42@gmail.com>
2019-01-16 15:20:10 +0100

    feat(bypass-quota): Add a new var for quota bypassing

--- script/weather.sh
+++ script/weather.sh
@@ -1,4 +1,8 @@
-#temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }' )
-temp=32
+if [ -z "$FORCE_TEMP" ]
+then
+  temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }' )
+else
+  temp=$FORCE_TEMP
+fi
 
 echo -n "weather,city=clermont-ferrand temp=$temp"
