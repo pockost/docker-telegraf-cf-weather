fa90b489adf11335412ad4b31e9bf56803a3922c
Romain THERRAT <romain42@gmail.com>
2019-07-03 17:43:27 +0200

    fix(weather): when to value is returned

--- script/weather.sh
+++ script/weather.sh
@@ -1,6 +1,6 @@
 if [ -z "$FORCE_TEMP" ]
 then
-  temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }' )
+  temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }'|cut -d'.' -f1)
 else
   temp=$FORCE_TEMP
 fi
