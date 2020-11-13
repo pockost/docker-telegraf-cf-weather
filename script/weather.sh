32d8d07bed9d6226aa48517457ec95900ae24ac2
Romain THERRAT <romain@pockost.com>
2020-11-13 11:59:42 +0100

    feat(temp): Add RANDOM_TEMP env var

--- script/weather.sh
+++ script/weather.sh
@@ -1,6 +1,11 @@
 if [ -z "$FORCE_TEMP" ]
 then
-  temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|awk '{ print $(NF-1) }'|cut -d'.' -f1)
+  if [ -z "$RANDOM_TEMP" ]
+  then
+    temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|tail -n1|awk '{ print $(NF-1) }'|cut -d'.' -f1)
+  else
+    temp=$(( $RANDOM % 30 ))
+  fi
 else
   temp=$FORCE_TEMP
 fi
