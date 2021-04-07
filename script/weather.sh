b1bc0d3cf39c8234bb42111dc92666990883dbc0
Romain THERRAT <romain@pockost.com>
2021-04-07 16:49:05 +0200

    feat(meteo): Handle if filling weather is return

--- script/weather.sh
+++ script/weather.sh
@@ -2,7 +2,7 @@ if [ -z "$FORCE_TEMP" ]
 then
   if [ -z "$RANDOM_TEMP" ]
   then
-    temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|tail -n1|awk '{ print $(NF-1) }'|cut -d'.' -f1)
+    temp=$( curl 'http://wttr.in/clermont-ferrand?0&Q&T' 2>/dev/null|grep C|tail -n1|awk '{ print $(NF-1) }'|cut -d'.' -f1|sed -e 's/^\([+-]*[0-9]*\).*$/\1/')
   else
     temp=$(( $RANDOM % 30 ))
   fi
